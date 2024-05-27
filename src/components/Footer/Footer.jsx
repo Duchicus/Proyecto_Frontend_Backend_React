5f273195f22af946fd27c98d35efc4f959f60927
Duchicus <mpiernash@gmail.com>
2024-05-27 18:11:15 +0200

    footer view updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-dark text-light pt-4 pb-4">
+      <div className="container">
+        <div>
+          <div className="mx-auto">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">E-commerce</h5>
+            <p>
+              <a className="text-light" style={{ textDecoration: 'none' }}> mpiernash@gmail.com</a>
+            </p>
+            <p>
+              <a href="https://www.linkedin.com/in/manel-piernas-hernandez/" className="text-light" style={{ textDecoration: 'none' }}> https://www.linkedin.com/in/manel-piernas-hernandez/</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
