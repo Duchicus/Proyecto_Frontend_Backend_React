676c6db78c9f80ee5754fe82988b166ab8373475
Duchicus <mpiernash@gmail.com>
2024-05-31 13:21:53 +0200

    cart view updated

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from 'react'
 import OrderService from "../../service/OrderService";
 import { UserContext } from '../../context/UsersContext';
-import { Button, notification, Empty } from 'antd';
+import { notification, Empty } from 'antd';
 import { CartContext } from '../../context/CartsContext';
+import { MdOutlineShoppingBag } from "react-icons/md";
 import "./Cart.scss"
 
 const Cart = () => {
@@ -51,8 +52,7 @@ const Cart = () => {
 
   const buyIt = async () => {
     if (user && cartProducts.length > 0) {
-      let productsIds = cartProducts.map((product)=>product.id)
-      console.log(user);
+      let productsIds = cartProducts.map((product) => product.id)
       const productBuy = {
         UserId: user.id,
         ProductId: productsIds
@@ -62,9 +62,13 @@ const Cart = () => {
       return notification.success({
         message: 'Succesfully purchased'
       });
-    } else {
+    } else if (!user) {
       return notification.error({
         message: 'You must login for buy'
+      });
+    } else if (cartProducts.length === 0) {
+      return notification.error({
+        message: 'You must buy something'
       });
     }
   };
@@ -86,17 +90,14 @@ const Cart = () => {
             </div>
             {cartProducts.length > 0 ? (
               <div className="card-body">
-                <div>
-                  <ul>
-                    {Object.keys(countMap).map((productName, index) => (
-                      <li key={index}>
-                        <span>x{countMap[productName]} {productName}</span>
-                        <button onClick={() => addCart(productName)}>+</button>
-                        <button onClick={() => removeOneCart(productName)}>-</button>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+                {Object.keys(countMap).map((productName, index) => (
+                  <p key={index}>
+                    <span> {productName}</span>
+                    <button className='addRemoveButton bg-dark text-light' onClick={() => addCart(productName)}>+</button>
+                    {countMap[productName]}
+                    <button className='addRemoveButton bg-dark text-light' onClick={() => removeOneCart(productName)}>-</button>
+                  </p>
+                ))}
               </div>) : (
               <div className="card-body">
                 <div>
@@ -105,7 +106,14 @@ const Cart = () => {
               </div>
             )}
             <div className='card-footer-cart d-flex justify-content-between align-items-center money positive'>
-              <Button className='bg-dark text-light m-4' onClick={buyIt}>Buy</Button>
+              <button className="Btn">
+                <div className="sign">
+                  <svg viewBox="0 0 512 512" className="store-icon">
+                    <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
+                  </svg>
+                </div>
+                <div className="text" onClick={buyIt}>Buy</div>
+              </button>
               <span className='amount p-4'>{totalPrice()} <span className='currency-symbol'>$</span></span>
             </div>
           </div>
